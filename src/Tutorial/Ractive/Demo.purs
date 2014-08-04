41defc481695304c5641b255ef9b7c5b7beae83c
Aitor P. Iturri <aitor.iturri@gmail.com>
2014-08-04 21:46:54 +0200

    Added test

--- src/Tutorial/Ractive/Demo.purs
+++ src/Tutorial/Ractive/Demo.purs
@@ -1,4 +1,4 @@
-module Tutorial.Ractive.Main where
+module Tutorial.Ractive.Demo where
 
 import Control.Monad.Eff
 import qualified Control.Monad.Eff.Ractive as Ract
@@ -10,7 +10,7 @@ import Control.Monad.Cont.Trans
 import Data.Maybe
 import Network.HTTP
 import Network.XHR
-import Tutorial.Ractive
+import Tutorial.Ractive.Demo.Tutorials
 
 type Template = String
 
@@ -33,7 +33,7 @@ type TutorialConfigEff = Eff(xhr::XHR,ractiveM::Ract.RactiveM,trace::Trace)
 ractiveTemplate = "#ractive-template"
 ractiveElement = "ractive-element"
 
-tutorials = [{"name": "tut1"},{"name": "tut2"}]
+--tutorials = [{"name": "tut1"},{"name": "tut2"}]
 
 template :: String -> String -> URI -> URI -> TutorialConfig
 template element template baseUri dir = {contentTemplate: content, outputTemplate: output, element: element, template: template}
@@ -51,14 +51,17 @@ getFrom :: forall r. URI -> (RequestConfig r -> EffXHR r Unit)
 getFrom url = (flip $ flip get url) {}
 
 getTemplate :: forall e a b. (String -> a -> b) -> URI -> a -> ContT Unit (Eff (xhr::XHR | e)) b
-getTemplate fun uri = \v -> ContT \next -> do
-  getFrom uri $ defaultConfig {onLoadEnd = \response -> next $ fun (responseText response) v}
+getTemplate fun uri = \v ->
+  ContT \next -> do
+    getFrom uri $ defaultConfig {onLoadEnd = \response -> next $ fun (responseText response) v}
 
 outputPartialTut :: forall e. URI -> Unit -> ContT Unit (Eff (xhr::XHR | e)) (String -> TutorialPartials)
-outputPartialTut = getTemplate \tmpl -> \_ -> tutorialPartials tmpl
+outputPartialTut = getTemplate \tmpl ->
+  \_ -> tutorialPartials tmpl
 
 contentPartialTut :: forall e. URI -> (String -> TutorialPartials) -> ContT Unit (Eff (xhr::XHR | e)) TutorialPartials
-contentPartialTut = getTemplate \tmpl -> \fun -> fun tmpl
+contentPartialTut = getTemplate \tmpl ->
+  \fun -> fun tmpl
 
 ractiveTut :: forall e. TutorialConfig -> TutorialPartials -> ContT Unit (Eff (ractiveM::Ract.RactiveM | e)) Ract.Ractive
 ractiveTut config partials = ContT \next -> do
@@ -71,15 +74,24 @@ ractiveTut config partials = ContT \next -> do
 --  Ract.setPartial "contentP" partials.content ractive
 
 loadTutorial :: forall e. TutorialConfig -> Unit -> ContT Unit (Eff (ractiveM::Ract.RactiveM,xhr::XHR | e)) Ract.Ractive
-loadTutorial config = outputPartialTut config.outputTemplate >=> contentPartialTut config.contentTemplate >=> ractiveTut config
+loadTutorial config = outputPartialTut
+  config.outputTemplate >=>
+  contentPartialTut config.contentTemplate >=>
+  ractiveTut config
 
-launch :: forall e. TutorialConfig -> Eff (trace :: Trace, ractiveM :: Ract.RactiveM, xhr :: XHR | e) Unit
-launch tutorial = runContT (loadTutorial tutorial unit) $ \r ->
+launch :: forall e1 e2. Tutorial Unit e2 -> Eff (ractiveM::Ract.RactiveM,xhr::XHR,trace::Trace | e1) Unit
+launch (Tutorial name tutorialF) = runContT executeTutorial $ \r ->
   trace "DONE"
+  where
+    executeTutorial = loadTutorial (templateTuto name) >=> tutorialF
+
+--runContT (loadTutorial tutorial unit) $ \r ->
+--  trace "DONE"
 
 init = do
   r <- Ract.ractive "#ractive-nav-template" "ractive-nav" {tutorials: tutorials}
   flip (Ract.on "loadtutorial") r \r -> do
-    launch templateTuto1
-    trace "Cargado"
+    launch tutorial1
+    --launch templateTuto1
+    --trace "Cargado"
   trace "Initialization done"
--- src/Tutorial/Ractive/Tut1.purs
+++ src/Tutorial/Ractive/Tut1.purs
@@ -1,10 +1,19 @@
-module Tutorial.Ractive where
+module Tutorial.Ractive.Demo.Tutorials where
 
 import Debug.Trace
+import Control.Monad.Cont.Trans
 import Control.Monad.Eff
 import Control.Monad.Eff.Ractive
 
-tutorial1 :: Eff (trace :: Trace, ractiveM :: RactiveM) Ractive -> Eff (trace :: Trace, ractiveM :: RactiveM) Unit
-tutorial1 eff = do
+data Tutorial a eff = Tutorial String (Ractive -> ContT Unit (Eff eff) a)
+
+tutorials = [{"name": "tut1"},{"name": "tut2"}]
+
+tutorial1 :: Tutorial Unit (trace :: Trace, ractiveM :: RactiveM)
+tutorial1 = Tutorial "tut1" tutorial1Fn
+
+tutorial1Fn :: Ractive -> ContT Unit (Eff (trace :: Trace, ractiveM :: RactiveM)) Unit
+tutorial1Fn ractive = ContT \_ -> do
   trace "Tutorial 1 starting"
+  set "d" "d" ractive
   trace "Tutorial 1 Done"
